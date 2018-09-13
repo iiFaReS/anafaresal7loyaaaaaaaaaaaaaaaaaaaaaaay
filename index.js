7a1c7a69790c81517e4a6ad624b10a4616dd36ac
iiFaReS <42453501+iiFaReS@users.noreply.github.com>
2018-09-14 01:47:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -353,4 +353,4 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN); //want my token ? nope 
